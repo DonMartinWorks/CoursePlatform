6b27e7fdd893679ddfa803b39e897ccf73d8b522
DonMartinWorks <donmartinworks@gmail.com>
2025-04-16 11:48:58 -0400

    [SLUG] - Correción del archivo Js

--- resources/js/assets/string_to_slug.js
+++ resources/js/assets/string_to_slug.js
@@ -1,5 +1,5 @@
 /**
- * Converts a given string into a URL-friendly slug and sets it as the value of a specified HTML input element. (Version 1.0)
+ * Converts a given string into a URL-friendly slug and sets it as the value of a specified HTML input element. (Version 1.1)
  *
  * @param {string} str The input string to be converted into a slug.
  * @param {string} querySelector A CSS selector string used to target the HTML element whose value will be set to the generated slug.
@@ -7,16 +7,24 @@
 window.string_to_slug = function (str, querySelector) {
     str = str.replace(/^\s+|\s+$/g, '');
     str = str.toLowerCase();
-    var from = "àáäâèéëêìíïîùúüûñç./_,:;";
+
+    var from = "àáäâèéëêìíïîòóöôùúüûñç./_,:;";
     var to = "aaaaeeeeiiiioooouuuunc------";
 
-    for (var i = 0, l = from.length; i < l; i++) {
-        str = str.replace(new RegExp(from.charAt(i)))
+    var newStr = "";
+    for (var i = 0, l = str.length; i < l; i++) {
+        var char = str.charAt(i);
+        var index = from.indexOf(char);
+        if (index > -1) {
+            newStr += to.charAt(index);
+        } else {
+            newStr += char;
+        }
     }
 
-    str = str.replace(/[^a-z0-9 -]/g, '')
-        .replace(/\s+/g, '-')
+    newStr = newStr.replace(/\s+/g, '-')
+        .replace(/[^a-z0-9-]/g, '')
         .replace(/-+/g, '-');
 
-    document.querySelector(querySelector).value = str;
+    document.querySelector(querySelector).value = newStr;
 }
